c6e19758b93a89fac12b35192bba9dcd5ca16ec4
jiandi1027 <494716102@qq.com>
2018-05-04 11:32:34 +0800

    代码自动生成架包、配置文件、类

--- src/main/java/com/cjdjyf/newssm/utils/MyUtils.java
+++ src/main/java/com/cjdjyf/newssm/utils/MyUtils.java
@@ -62,4 +62,14 @@ public class MyUtils {
     public void test() {
         System.out.println(GetMD5("池剑迪", "1"));
     }
+
+    @Test
+    public void tes1t() throws Exception {
+        try {
+            MapperCreate generatorSqlmap = new MapperCreate("sys_group","com.cjdjyf.newssm.sys.mapper","com.cjdjyf.newssm.sys.pojo","com.cjdjyf.newssm.base.BaseDao");
+            generatorSqlmap.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
